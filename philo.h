5dd326b6430b1c341343d99b135045a841e0f6d2
alix4227 <alix_07@hotmail.fr>
2025-05-13 17:07:04 +0200

    v1

---
+++ philo.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrusoe <acrusoe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 14:56:04 by acrusoe           #+#    #+#             */
+/*   Updated: 2025/05/09 14:56:04 by acrusoe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_death
+{
+	int				dead;
+	int				done_eating;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	death;
+	pthread_mutex_t	done;
+}	t_death;
+
+typedef struct s_data
+{
+	long			i;
+	long			nb;
+	long			last_meal;
+	long			meal;
+	long			end;
+	long			ttd;
+	long			tts;
+	long			tte;
+	long			nb_time_eat;
+	long			time_now;
+	long			has_counted;
+	int				ac;
+	t_death			*check;
+	pthread_mutex_t	*fourchs;
+	pthread_mutex_t	mutex;
+}	t_data;
+
+#endif
